25d45cbf1dacaf3480bf4d086c762846d2797bf2
Aaditya Gupta <aadtiyagupta@itgeeks.com>
2025-06-13 17:03:53 +0530

    cors installed

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 const errorHandler = require("./middleware/errorHandler");
 const connectDb =require("./config/dbConnection");
+const cors = require("cors");
 
 connectDb();
 const app = express();
 const port =process.env.PORT||5000;
-
+app.use(cors())
 app.use(express.json());
 app.use("/api/contacts",require("./routes/contactRoutes"));
 app.use("/api/user",require("./routes/userRoutes"));
